3465ffb8bb4337456e09a7bab1e168f34515b1c0
johndward01 <johndward01@gmail.com>
2022-09-05 21:46:11 -0500

    Add project files.

---
+++ GuolongLi_Methods/Methods.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GuolongLi_Methods;
+internal class Methods
+{
+    public static int Add(int num1, int num2)
+    {
+        return num1 + num2;
+    }
+
+    public static decimal Add(decimal num1, decimal num2)
+    {
+        return num1 + num2;
+    }
+}
---
+++ GuolongLi_Methods/Program.cs
@@ -0,0 +1,5 @@
+﻿using GuolongLi_Methods;
+
+Methods.Add(1, 1);
+Methods.Add(1.99m, 10.99m);
+
